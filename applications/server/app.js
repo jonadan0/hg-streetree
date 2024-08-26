7be8608e96f94a5f3274d3f7d0e0fb9d3f90aeb0
NoobTh <n006h3417h@gmail.com>
2024-08-26 21:09:01 +0900

    Update app.js

--- applications/server/app.js
+++ applications/server/app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/farm", farmRoutes);
 app.use("/auth", authRoutes);
 
-const port = process.env.PORT || 6974;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server on port ${port}`);
 });
