0d9f9e3fa3e7779711f3078d19fbc3e6aa25629c
NoobTh <n006h3417h@gmail.com>
2024-08-27 10:49:10 +0900

    Update database

--- applications/server/utils/db.js
+++ applications/server/utils/db.js
@@ -2,10 +2,10 @@ const sql = require("mssql");
 const dbconfig = require("../config/dbconfig.json");
 
 const config = {
-  user: dbconfig.user,
-  password: dbconfig.password,
-  server: dbconfig.server,
-  database: dbconfig.database,
+  user: process.env.AZDB_USERNAME,
+  password: dprocess.env.AZDB_PASSWORD,
+  server: process.env.AZDB_SERVER,
+  database: process.env.AZDB_DATABASE,
   authentication: {
     type: "default",
   },
